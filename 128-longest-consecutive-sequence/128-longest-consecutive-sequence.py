6206e8814179c692827a2b073a313eeac424510d
PradyotC <48565460+PradyotC@users.noreply.github.com>
2022-07-05 09:32:07 -0700

    Time: 449 ms (72.68%), Space: 28 MB (72.70%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -5,9 +5,11 @@ class Solution:
         count = 1
         for i in nums:
             if i-1 not in nums:
-                c = 0
-                while i+c in nums: 
+                c = 1
+                j = i
+                while j+1 in nums: 
                     c+=1
+                    j+=1
                 count = max(count,c)
         return count
         

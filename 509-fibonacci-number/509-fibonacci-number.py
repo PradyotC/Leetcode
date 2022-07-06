26e90f153e842eaca6b9762fa1b59af3d402b8c4
PradyotC <48565460+PradyotC@users.noreply.github.com>
2022-07-06 15:12:18 -0700

    Time: 28 ms (96.90%), Space: 13.7 MB (99.74%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,10 +1,10 @@
 class Solution:
-    def __init__(self):
-        self.dictt = {
-            0:0,
-            1:1
-        }
     def fib(self, n: int) -> int:
-        if n in self.dictt: return self.dictt[n]
-        self.dictt[n] = self.fib(n-1)+self.fib(n-2)
-        return self.dictt[n]
+        if n<2:
+            return n
+        ans = [0,1]
+        while n>2:
+            ans.insert(1,ans.pop(0))
+            ans[1] = sum(ans)
+            n-=1
+        return sum(ans)

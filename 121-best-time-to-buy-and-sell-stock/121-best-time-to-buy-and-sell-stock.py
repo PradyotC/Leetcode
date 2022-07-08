889dc5b460c3a3c3452fdfc9ec7a6f60e9df4ad2
PradyotC <48565460+PradyotC@users.noreply.github.com>
2022-07-08 13:29:22 -0700

    Time: 1152 ms (85.08%), Space: 25.1 MB (6.63%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,8 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        l = 0
+        low = prices[0]
         prof = 0
         for r in range(1,len(prices)):
-            if prices[r]<prices[l]: l=r #Here when profit is negative means we have found the newest low point
-            prof = max(prof,prices[r]-prices[l])
+            if prices[r]<low: low=prices[r] #Here when profit is negative means we have found the newest low point
+            prof = max(prof,prices[r]-low)
         return prof

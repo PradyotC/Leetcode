016eb7c0917b030343f4e33a3fc5d5b781860ea8
PradyotC <48565460+PradyotC@users.noreply.github.com>
2022-07-09 15:48:34 -0700

    Time: 2619 ms (28.08%), Space: 15.2 MB (23.74%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -5,7 +5,7 @@ class Solution:
         for i in range(n-2,-1,-1):
             minn = min(n,nums[i]+i+1)
             for j in range(i+1,minn):
-                ans[i] = min(ans[i],ans[j])
+                if ans[j]<ans[i]: ans[i]=ans[j]
             ans[i] += 1
         return ans[0]
                 

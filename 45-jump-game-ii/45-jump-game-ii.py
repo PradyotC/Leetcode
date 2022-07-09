18a725a36ef2c96b12542e7066752861b2a7b28e
PradyotC <48565460+PradyotC@users.noreply.github.com>
2022-07-09 15:49:53 -0700

    Time: 2234 ms (29.18%), Space: 15.1 MB (23.74%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -3,7 +3,8 @@ class Solution:
         n = len(nums)
         ans = [10001]*(n-1)+[0]
         for i in range(n-2,-1,-1):
-            minn = min(n,nums[i]+i+1)
+            if n<nums[i]+i+1: minn = n
+            else: minn = nums[i]+i+1
             for j in range(i+1,minn):
                 if ans[j]<ans[i]: ans[i]=ans[j]
             ans[i] += 1

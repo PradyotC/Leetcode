87e1e1bc8f34866eb8a85742658afe72a21f880f
PradyotC <48565460+PradyotC@users.noreply.github.com>
2022-07-09 15:42:24 -0700

    Time: 7967 ms (20.38%), Space: 15 MB (89.56%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,8 +1,7 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         n = len(nums)
-        ans = [10001]*n
-        ans[-1]=0
+        ans = [10001]*(n-1)+[0]
         for i in range(n-2,-1,-1):
             minn = min(n,nums[i]+i+1)
             for j in range(i+1,minn):

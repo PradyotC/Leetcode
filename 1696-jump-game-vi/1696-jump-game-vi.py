cc2be224f7eb8b3e2d1e8d13efe5b8f80e533445
PradyotC <48565460+PradyotC@users.noreply.github.com>
2022-07-13 12:37:14 -0700

    Time: 1390 ms (73.68%), Space: 27.8 MB (72.73%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.py
+++ 1696-jump-game-vi/1696-jump-game-vi.py
@@ -1,16 +1,14 @@
 class Solution:
     def maxResult(self, nums: List[int], k: int) -> int:
         n = len(nums)
-        score = [0]*n
-        score[0] = nums[0]
-        priority_queue = []
-        # since heapq is a min-heap,
-        # we use negative of the numbers to mimic a max-heap
-        heapq.heappush(priority_queue, (-nums[0], 0))
-        for i in range(1, n):
-            # pop the old index
-            while priority_queue[0][1] < i-k:
-                heapq.heappop(priority_queue)
-            score[i] = nums[i]+score[priority_queue[0][1]]
-            heapq.heappush(priority_queue, (-score[i], i))
-        return score[-1]
+        dq = deque()
+        score = nums[0]
+        dq.append((0,score))
+        for i in range(1,n):
+            while dq and dq[0][0] < i-k:
+                dq.popleft()
+            score = nums[i]+dq[0][1]
+            while dq and dq[-1][1] <= score:
+                dq.pop()
+            dq.append((i,score))
+        return score
